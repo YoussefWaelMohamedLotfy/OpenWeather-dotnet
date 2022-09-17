4aaf78e8cd7173bf7fcfbd4bd83a2e96247549fd
Youssef Wael Mohamed-Lotfy Mousa <40206862+YoussefWaelMohamedLotfy@users.noreply.github.com>
2022-09-17 02:13:32 +0200

    Added GetWeather Endpoint

---
+++ src/OpenWeather.SDK/SdkExtensions.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using OpenWeather.SDK.Weather;
+using Refit;
+
+namespace OpenWeather.SDK;
+
+/// <summary>
+/// Extensions to the SDK
+/// </summary>
+public static class SdkExtensions
+{
+    /// <summary>
+    /// Registers the OpenWeather API to the DI Container
+    /// </summary>
+    /// <param name="services">DI Container</param>
+    /// <returns>A reference to the Service Collection for Chaining Calls</returns>
+    public static IServiceCollection AddOpenWeather(this IServiceCollection services)
+    {
+        services.AddRefitClient<IWeatherApi>()
+            .ConfigureHttpClient(client =>
+            {
+                client.BaseAddress = new Uri("https://api.openweathermap.org/data/2.5");
+            });
+
+        return services;
+    }
+}
---
+++ src/OpenWeather.SDK/Weather/IWeatherApi.cs
@@ -0,0 +1,15 @@
+﻿using Refit;
+
+namespace OpenWeather.SDK.Weather;
+
+public interface IWeatherApi
+{
+    /// <summary>
+    /// Gets Current Weather for a city
+    /// </summary>
+    /// <param name="city">City name, State Code, Country Code</param>
+    /// <param name="apiKey">Your API Key for authenticating the request</param>
+    /// <returns>The Weather Details for the specified City</returns>
+    [Get("/weather")]
+    Task<IApiResponse> GetWeatherAsync([AliasAs("q")] string city, [AliasAs("appid")] string apiKey);
+}
