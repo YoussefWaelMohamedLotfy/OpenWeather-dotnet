290f04576f3c2aa339607d1433649b4a24c4d4fb
Youssef Wael Mohamed-Lotfy Mousa <40206862+YoussefWaelMohamedLotfy@users.noreply.github.com>
2022-09-18 12:24:27 +0200

    Added Samples for Geocoding

--- samples/OpenWeather.Console/Program.cs
+++ samples/OpenWeather.Console/Program.cs
@@ -1,9 +1,15 @@
-﻿using OpenWeather.SDK.Weather;
+﻿using OpenWeather.SDK.Geocoding;
+using OpenWeather.SDK.Weather;
 using Refit;
 
 Console.WriteLine("Testing API Endpoints...");
 
-var weatherApi = RestService.For<IWeatherApi>("");
-var response = await weatherApi.GetWeatherAsync("London", "YOUR-KEY");
+var weatherApi = RestService.For<IWeatherApi>("https://api.openweathermap.org/data/2.5");
+var weatherResponse = await weatherApi.GetWeatherAsync("London", "YOUR-KEY");
 
-Console.WriteLine(response.StatusCode);
+Console.WriteLine($"Weather API Response Code: {weatherResponse.StatusCode}");
+
+var geocodingApi = RestService.For<IGeocodingApi>("https://api.openweathermap.org/geo/1.0");
+var geocodingResponse = await geocodingApi.GetCityCoordinatesAsync("London", "6e2cf622ef8f02a604c95267d04473e3", 1);
+
+Console.WriteLine($"Geocoding API Response Code: {geocodingResponse.StatusCode}");
