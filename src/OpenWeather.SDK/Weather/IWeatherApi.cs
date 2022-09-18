d480029b9025bdaa2cc15025f31a10bbc6c88fdc
Youssef Wael Mohamed-Lotfy Mousa <40206862+YoussefWaelMohamedLotfy@users.noreply.github.com>
2022-09-18 14:31:14 +0200

    Added new Endpoint to WeatherApi

--- src/OpenWeather.SDK/Weather/IWeatherApi.cs
+++ src/OpenWeather.SDK/Weather/IWeatherApi.cs
@@ -8,11 +8,21 @@ namespace OpenWeather.SDK.Weather;
 public interface IWeatherApi
 {
     /// <summary>
-    /// Gets Current Weather for a city
+    /// Gets Current Weather for a city by Name
     /// </summary>
     /// <param name="city">City name, State Code, Country Code</param>
     /// <param name="apiKey">Your API Key for authenticating the request</param>
     /// <returns>The Weather Details for the specified City</returns>
     [Get("/weather")]
     Task<IApiResponse> GetWeatherAsync([AliasAs("q")] string city, [AliasAs("appid")] string apiKey);
+
+    /// <summary>
+    /// Gets Current Weather for a city by Coordinates
+    /// </summary>
+    /// <param name="latitude">City or Area Latitude</param>
+    /// <param name="longitude">City or Area Longitude</param>
+    /// <param name="apiKey">Your API Key for authenticating the request</param>
+    /// <returns>The Weather Details for the specified City</returns>
+    [Get("/weather")]
+    Task<IApiResponse> GetWeatherAsync([AliasAs("lat")] string latitude, [AliasAs("lon")] string longitude, [AliasAs("appid")] string apiKey);
 }
